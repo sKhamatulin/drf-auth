5d520e442846e81bb1fc294d93f776693a6c314a
Sergey Khamatulin <Khamatulin24@gmail.com>
2024-09-22 18:07:32 +0500

    hooks realisation

--- drf_auth/api/urls.py
+++ drf_auth/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
-from .views import AuthStatusView, UserViewSet
+from .views import (AuthStatusView, UserViewSet, UserContactView,
+                    UserManagerView, UserCompaniesView, UserCompanyDetailsView)
 from rest_framework.routers import DefaultRouter
 
 app_name = 'api'
@@ -8,13 +9,25 @@ router = DefaultRouter()
 router.register(r'users', UserViewSet, basename='user')
 
 urlpatterns = [
-    path('v1/auth/', include('djoser.urls')),
-    path('v1/auth/', include('djoser.urls.jwt')),
-    path('v1/auth/status/',
-         AuthStatusView.as_view(),
-         name='auth-status'),
-    path('v1/auth/me/',
-         UserViewSet.as_view({'get': 'retrieve'}),
-         name='user-me'),
-    path('', include(router.urls)),
+     path('v1/auth/', include('djoser.urls')),
+     path('v1/auth/', include('djoser.urls.jwt')),
+     path('v1/auth/status/',
+          AuthStatusView.as_view(),
+          name='auth-status'),
+     path('v1/auth/me/',
+          UserViewSet.as_view({'get': 'retrieve'}),
+          name='user-me'),
+     path('v1/hooks/users_me_contact/',
+          UserContactView.as_view(),
+          name='user-contact'),
+     path('v1/hooks/users_me_manager/',
+          UserManagerView.as_view(),
+          name='user-manager'),
+     path('v1/hooks/company_list/',
+          UserCompaniesView.as_view(),
+          name='company-list'),
+     path('v1/hooks/user_me_company/',
+          UserCompanyDetailsView.as_view(),
+          name='user-company-details'),
+     path('', include(router.urls)),
 ]
--- drf_auth/api/views.py
+++ drf_auth/api/views.py
@@ -1,3 +1,5 @@
+import requests
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
@@ -7,6 +9,81 @@ from django.http import HttpResponse
 import yaml
 
 from .serializers import UserSerializer
+
+
+WEBHOOK_TOKEN = 'fnhhlfuj12fos91o'
+
+
+class UserContactView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+
+        if not user.contactId:
+            return Response({'error': 'contactId not found.'}, status=400)
+
+        # contact_id = user.contactId
+        contact_id = 9
+
+        url = f'https://b-p24.ru/rest/7/{WEBHOOK_TOKEN}/crm.contact.get.json?id={contact_id}'
+
+        response = requests.get(url)
+
+        return Response(response.json(), status=response.status_code)
+
+
+class UserManagerView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user_contact_view = UserContactView()
+        data = user_contact_view.get(request).data
+        assigned_by_id = data.get('result', {}).get('ASSIGNED_BY_ID')
+
+        url = f'https://b-p24.ru/rest/7/{WEBHOOK_TOKEN}/crm.contact.get.json?id={assigned_by_id}'
+
+        response = requests.get(url)
+
+        return Response(response.json(), status=response.status_code)
+
+
+class UserCompaniesView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user_contact_view = UserContactView()
+        data = user_contact_view.get(request).data
+        user_id = data.get('result', {}).get('ID')
+
+        url = f'https://b-p24.ru/rest/7/{WEBHOOK_TOKEN}/crm.contact.company.items.get.json?id={user_id}'
+
+        response = requests.get(url)
+
+        return Response(response.json(), status=response.status_code)
+    
+
+class UserCompanyDetailsView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user_companies_view = UserCompaniesView()
+        companies_response = user_companies_view.get(request)
+        company_id_list = [company['COMPANY_ID'] for company in
+                           companies_response.data.get('result', [])]
+        company_details_list = []
+
+        for company_id in company_id_list:
+            url = f'https://b-p24.ru/rest/7/{WEBHOOK_TOKEN}/crm.company.get.json?id={company_id}'
+            response = requests.get(url)
+
+            if response.status_code == 200:
+                company_details_list.append(response.json().get('result', {}))
+            else:
+                company_details_list.append({'error': 'Error fetching company'
+                                             f'{company_id}: {response.text}'})
+
+        return Response(company_details_list, status=200)
 
 
 class AuthStatusView(APIView):
