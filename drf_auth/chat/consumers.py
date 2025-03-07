34d2acfbf3a72d2ef8d669886a1ae692ff467a30
Sergey Khamatulin <Khamatulin24@gmail.com>
2025-03-07 16:34:01 +0500

    ws connected fix localhost

--- drf_auth/chat/consumers.py
+++ drf_auth/chat/consumers.py
@@ -1,10 +1,14 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
-from .models import ChatRoom, Message
-
+from django.apps import apps
+from channels.db import database_sync_to_async
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
+        # Ленивая загрузка моделей
+        self.ChatRoom = apps.get_model('chat.ChatRoom')
+        self.Message = apps.get_model('chat.Message')
+
         self.chat_room_id = self.scope['url_route']['kwargs']['chat_room_id']
         self.room_group_name = f'chat_{self.chat_room_id}'
 
@@ -24,30 +28,35 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
     # Получение сообщения от WebSocket
     async def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
-        sender_type = text_data_json['sender_type']
-        contactId = text_data_json['contactId']  # Используем contactId из CustomUser
+        try:
+            text_data_json = json.loads(text_data)
+            message = text_data_json['message']
+            sender_type = text_data_json['sender_type']
+            contactId = text_data_json['contactId']  # Используем contactId из CustomUser
 
-        # Сохраняем сообщение в базе данных
-        chat_room = await ChatRoom.objects.aget(id=self.chat_room_id)
-        await Message.objects.acreate(
-            chat_room=chat_room,
-            sender_type=sender_type,
-            contactId=contactId,
-            content=message
-        )
+            # Логируем полученные данные
+            print(f"Получено сообщение: {message}, sender_type: {sender_type}, contactId: {contactId}")
 
-        # Отправляем сообщение в группу
-        await self.channel_layer.group_send(
-            self.room_group_name,
-            {
-                'type': 'chat_message',
-                'message': message,
-                'sender_type': sender_type,
-                'contactId': contactId,
-            }
-        )
+            # Сохраняем сообщение в базе данных
+            chat_room = await self.get_chat_room(self.chat_room_id)
+
+            created_message = await self.create_message(chat_room, sender_type, contactId, message)
+            print(f"Сообщение сохранено в базе данных. ID сообщения: {created_message.id}")
+
+            # Отправляем сообщение в группу
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    'type': 'chat_message',
+                    'message': message,
+                    'sender_type': sender_type,
+                    'contactId': contactId,
+                }
+            )
+
+        except Exception as e:
+            print(f"Ошибка при обработке сообщения: {e}")
+            await self.close()
 
     # Получение сообщения из группы
     async def chat_message(self, event):
@@ -55,9 +64,25 @@ class ChatConsumer(AsyncWebsocketConsumer):
         sender_type = event['sender_type']
         contactId = event['contactId']
 
+        # Логируем отправку сообщения обратно в WebSocket
+        print(f"Отправка сообщения в WebSocket: {message}, sender_type: {sender_type}, contactId: {contactId}")
+
         # Отправляем сообщение обратно в WebSocket
         await self.send(text_data=json.dumps({
             'message': message,
             'sender_type': sender_type,
             'contactId': contactId,
         }))
+
+    @database_sync_to_async
+    def get_chat_room(self, chat_room_id):
+        return self.ChatRoom.objects.get(id=chat_room_id)
+
+    @database_sync_to_async
+    def create_message(self, chat_room, sender_type, contactId, message):
+        return self.Message.objects.create(
+            chat_room=chat_room,
+            sender_type=sender_type,
+            contactId=contactId,
+            content=message
+        )
